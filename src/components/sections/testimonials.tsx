75e7d0d23cbc44af0877885f7979120cb20e98c8
Aryan Mane <aryanmane7916@gmail.com>
2025-02-18 14:56:53 +0530

    fix: testimonials

--- src/components/sections/testimonials.tsx
+++ src/components/sections/testimonials.tsx
@@ -3,55 +3,55 @@ import { TestimonialsHeader } from "../testimonial-header";
 
 const testimonialsData = [
   {
-    imageUrl: "/nikhil.png",
+    imageUrl: "/avatars/nikhil.png",
     name: "Nikhil",
     rating: 5,
     testimonial: "Overall service was good. Rooms were clean, staff was chill and friendly. Its good for Couple, keeping in mind - Lodge location is bit into public area. But it was fine. They asked Rs. 1800/- for one day."
   },
   {
-    imageUrl: "Jitendra Singh.png",
+    imageUrl: "/avatars/Jitendra Singh.png",
     name: "Jitendra Singh",
     rating: 5,
     testimonial: "The hotel is very awesome to stay in good budget. Breakfast is also nice. Rooms are very big and clean and higenic. There are many windows to get clean air."
   },
   {
-    imageUrl: "Adarsh Ranjan.png",
+    imageUrl: "/avatars/Adarsh Ranjan.png",
     name: "Adarsh Ranjan",
     rating: 5,
     testimonial: "Best place to stay if you are in viman nagar very good hospitality and room are pretty much clear and hygienic. Must recommended for couples nice location and high security. The rooms are as same as shown in pictures."
   },
   {
-    imageUrl: "Ishan Mahajan.png",
+    imageUrl: "/avatars/Ishan Mahajan.png",
     name: "Ishan Mahajan",
     rating: 5,
     testimonial: "Great stay, rooms were very clean and amenities were good as well. Highly recommended."
   },
   {
-    imageUrl: "/Brijesh BShetty.png",
+    imageUrl: "/avatars/Brijesh BShetty.png",
     name: "Brijesh BShetty",
     rating: 5,
     testimonial: "My stay at the White Castle Heritage was a wonderful experience. I had to stay here for a few days and the staff and the owner of the hotel were really friendly and welcoming. I would definitely recommend this place to anyone looking for a budget stay around the viman nagar area. If I have to comeback. I would stay here for sure."
   },
   {
-    imageUrl: "Vinuth A.png",
+    imageUrl: "/avatars/Vinuth A.png",
     name: "Vinuth A",
     rating: 5,
     testimonial: "Staff from the hotel was very helpful and understanding and was always been supportive and ensured the room and washrooms were clean all the day."
   },
   {
-    imageUrl: "Piyush MilindKamble.png",
+    imageUrl: "/avatars/Piyush MilindKamble.png",
     name: "Piyush MilindKamble",
     rating: 5,
     testimonial: "The staff is well behaved. The rooms are spacious and just as shown in the image. The rooms are hygenic. The location is near to phoneix but a little hard to find, apart from that - great service."
   },
   {
-    imageUrl: "Uttam Raj.png",
+    imageUrl: "/avatars/Uttam Raj.png",
     name: "Uttam Raj",
     rating: 5,
     testimonial: "Nice place, no disturbance good receptionist, neatly maintained room, bathrooms. Cost also reasonable."
   },
   {
-    imageUrl: "Rajesh Mane.png",
+    imageUrl: "/avatars/Rajesh Mane.png",
     name: "Rajesh Mane",
     rating: 5,
     testimonial: "Very good Hotel for corporate & long stay. Very Spacious room with dinning table & balcony. Big bathroom with Jaguar fitting and very clean. Walking distance from Phoenix Mall"
