cc194e990f46eba731a7ddcc8ce9ecae7a93e2cf
Aryan Mane <aryanmane7916@gmail.com>
2025-02-19 12:18:46 +0530

    feat: hero

--- src/components/sections/hero.tsx
+++ src/components/sections/hero.tsx
@@ -1,4 +1,5 @@
 "use client"
+import { ChevronDown } from "lucide-react";
 import { LampContainer } from "../lamp"
 import { motion } from "framer-motion";
 
@@ -14,21 +15,55 @@ export const Hero = () => {
             duration: 0.8,
             ease: "easeInOut",
           }}
-          className="mt-28 bg-gradient-to-br from-slate-100 to-slate-300 bg-clip-text text-center text-4xl tracking-tight text-transparent font-bold md:text-7xl">
+          viewport={{ once: true }}
+          className="mt-28 mb-4 bg-gradient-to-br from-slate-100 to-slate-300 bg-clip-text text-center text-4xl tracking-tight text-transparent font-extrabold md:text-7xl">
             Welcome to <span className="text-orange-500">GAPSTAYS</span>
         </motion.h1>
+        
         <motion.div
           initial={{ opacity: 0, y: 100 }}
           whileInView={{ opacity: 1, y: 0 }}
           transition={{
-            delay: 0.3,
+            delay: 0.4,
             duration: 0.8,
             ease: "easeInOut",
-          }} className=" text-center">
-          <div className="text-4xl text-gray-200 font-bold mt-3">Experience Comfort Without Compromise</div>
-          <div className="text-gray-300 text-2xl font-medium mt-1">Your perfect stay awaits in the heart of the city</div>
+          }}
+          viewport={{ once: true }}
+          className="text-center space-y-3">
+          <motion.div 
+            initial={{ y: 30 }}
+            whileInView={{ y: 0 }}
+            transition={{
+              duration: 0.5,
+              delay: 0.4,
+              ease: [0.22, 1, 0.36, 1],
+            }}
+            viewport={{ once: true }}
+            className="text-4xl text-gray-200 font-bold">
+            Experience Comfort Without Compromise
+          </motion.div>
+          
+          <motion.div 
+            initial={{ y: 20 }}
+            whileInView={{ y: 0 }}
+            transition={{
+              duration: 0.5,
+              delay: 0.5,
+              ease: [0.22, 1, 0.36, 1],
+            }}
+            viewport={{ once: true }}
+            className="text-gray-300 text-2xl font-normal px-4">
+            Your perfect stay awaits in the heart of the city
+          </motion.div>
         </motion.div>
-    </LampContainer>
+
+        <div className="flex flex-col md:flex-row gap-4 justify-center items-center">
+            <a href="" className="bg-[#FF6B35] hover:bg-[#ff8557] text-white font-bold py-4 px-8 rounded-full transition-all duration-300 transform hover:scale-105 flex items-center gap-2"><span>Book Your Stay</span></a>
+            <a href="" className="bg-transparent border-2 border-white text-white hover:border-[#FF6B35] hover:text-[#FF6B35] font-bold py-4 px-8 rounded-full transition-all duration-300">Explore Gallery</a>
+        </div>
+
+        <div className="absolute bottom-10 left-1/2 transform -translate-x-1/2 hover:cursor-pointer"><ChevronDown/></div>
+      </LampContainer>
     </section>
   )
 }
