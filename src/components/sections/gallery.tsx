8167c2e21fe7e0d63490eb4dec0db4f47d58ca0c
Aryan Mane <aryanmane7916@gmail.com>
2025-02-22 19:31:52 +0530

    feat: gallery

--- src/components/sections/gallery.tsx
+++ src/components/sections/gallery.tsx
@@ -7,36 +7,36 @@ import NextImage from 'next/image';
 
 export const Gallery = () => {
   const images = [
-    "/gallery/Gapstays-2.jpg",
-    "/gallery/Gapstays-4.jpg",
-    "/gallery/Gapstays-8.jpg",
-    "/gallery/Gapstays-22.jpg",
-    "/gallery/Gapstays-24.jpg",
-    "/gallery/Gapstays-3.jpg",
-    "/gallery/Gapstays-5.jpg",
-    "/gallery/Gapstays-6.jpg",
-    "/gallery/Gapstays-7.jpg",
-    "/gallery/Gapstays-11.jpg",
-    "/gallery/Gapstays-12.jpg",
-    "/gallery/Gapstays-20.jpg",
-    "/gallery/Gapstays-15.jpg",
-    "/gallery/Gapstays-17.jpg",
-    "/gallery/Gapstays-14.jpg",
-    "/gallery/Gapstays-9.jpg",
-    "/gallery/Gapstays-21.jpg",
-    "/gallery/Gapstays-10.jpg",
-    "/gallery/Gapstays-26.jpg",
-    "/gallery/Gapstays-27.jpg",
-    "/gallery/Gapstays-28.jpg",
-    "/gallery/Gapstays-29.jpg",
-    "/gallery/Gapstays-36.jpg",
-    "/gallery/Gapstays-30.jpg",
-    "/gallery/Gapstays-31.jpg",
-    "/gallery/Gapstays-32.jpg",
-    "/gallery/Gapstays-34.jpg",
-    "/gallery/Gapstays-38.jpg",
-    "/gallery/Gapstays-37.jpg",
-    "/gallery/Gapstays-39.jpg",
+    "/gallery/Gapstays-2.jpg", //1
+    "/gallery/Gapstays-4.jpg", //2
+    "/gallery/Gapstays-8.jpg", //3
+    "/gallery/Gapstays-22.jpg", //4
+    "/gallery/Gapstays-24.jpg", //5
+    "/gallery/Gapstays-3.jpg", //6
+    // "/gallery/Gapstays-5.jpg",
+    // "/gallery/Gapstays-6.jpg",
+    // "/gallery/Gapstays-7.jpg",
+    // "/gallery/Gapstays-11.jpg",
+    // "/gallery/Gapstays-12.jpg",
+    // "/gallery/Gapstays-20.jpg",
+    // "/gallery/Gapstays-15.jpg",
+    // "/gallery/Gapstays-17.jpg",
+    // "/gallery/Gapstays-14.jpg",
+    // "/gallery/Gapstays-9.jpg",
+    // "/gallery/Gapstays-21.jpg",
+    // "/gallery/Gapstays-10.jpg",
+    // "/gallery/Gapstays-26.jpg",
+    // "/gallery/Gapstays-27.jpg",
+    // "/gallery/Gapstays-28.jpg",
+    // "/gallery/Gapstays-29.jpg",
+    // "/gallery/Gapstays-36.jpg",
+    // "/gallery/Gapstays-30.jpg",
+    // "/gallery/Gapstays-31.jpg",
+    // "/gallery/Gapstays-32.jpg",
+    // "/gallery/Gapstays-34.jpg",
+    // "/gallery/Gapstays-38.jpg",
+    // "/gallery/Gapstays-37.jpg",
+    // "/gallery/Gapstays-39.jpg",
   ];
 
   const [isModalOpen, setIsModalOpen] = useState(false);
