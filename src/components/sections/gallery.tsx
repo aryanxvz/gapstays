0fd775871fbdec4a3b4b8c10e8b278e1ffd011de
Aryan Mane <aryanmane7916@gmail.com>
2025-02-22 19:37:27 +0530

    feat: gallery

--- src/components/sections/gallery.tsx
+++ src/components/sections/gallery.tsx
@@ -13,12 +13,12 @@ export const Gallery = () => {
     "/gallery/Gapstays-22.jpg", //4
     "/gallery/Gapstays-24.jpg", //5
     "/gallery/Gapstays-3.jpg", //6
-    // "/gallery/Gapstays-5.jpg",
-    // "/gallery/Gapstays-6.jpg",
-    // "/gallery/Gapstays-7.jpg",
-    // "/gallery/Gapstays-11.jpg",
-    // "/gallery/Gapstays-12.jpg",
-    // "/gallery/Gapstays-20.jpg",
+    "/gallery/Gapstays-5.jpg",
+    "/gallery/Gapstays-6.jpg",
+    "/gallery/Gapstays-7.jpg",
+    "/gallery/Gapstays-11.jpg",
+    "/gallery/Gapstays-12.jpg",
+    "/gallery/Gapstays-20.jpg",
     // "/gallery/Gapstays-15.jpg",
     // "/gallery/Gapstays-17.jpg",
     // "/gallery/Gapstays-14.jpg",
