0e4edfb159749f1e7beab58996b7e48e4abcac2f
Aryan Mane <aryanmane7916@gmail.com>
2025-02-28 16:50:40 +0530

    feat: policy pages

---
+++ src/app/guest-policy/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Guest = () => {
+  return (
+    <section>
+    </section>
+  );
+};
+
+export default Guest;
---
+++ src/app/privacy-policy/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Privacy = () => {
+  return (
+    <section>
+    </section>
+  );
+};
+
+export default Privacy;
