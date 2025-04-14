8d822fca16dff3a134c7836cccf6c6fe8d41194f
Aryan Mane <aryanmane7916@gmail.com>
2025-04-14 14:41:34 +0530

    feat: analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from 'next/font/google'
 import "./globals.css";
+import { GoogleAnalytics } from '@next/third-parties/google'
 
 const inter = Inter({
   subsets: ["latin"]
@@ -17,6 +18,7 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
       <body className={`${inter.className} relative min-h-screen overflow-x-hidden bg-neutral-900`}>
         {children}
       </body>
+      <GoogleAnalytics gaId="G-SVHPLV5TPN" />
     </html>
   );
 }
